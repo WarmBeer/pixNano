bd88a33169501c8581ab7f6f496a55a99f993a30
WarmBeer <mickvd99@gmail.com>
2019-02-13 17:51:29 +0100

    Added chat and a cooldown for messages and pixels

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const CANVAS_COLS = 250
 const fs = require("fs")
 
 var canvas = [ ]
+var messages = {}
+var pixels = {}
 
 function saveCanvas() {
     fs.writeFile('canvas.txt', JSON.stringify(canvas), (err) => {
@@ -28,7 +30,7 @@ function loadCanvas() {
 
 function saveBackup() {
     var now = new Date();
-    fs.copyFile('canvas.txt', "backups/canvas - "+ now.getDate()+'-'+ now.getMonth()+'-'+ now.getFullYear()+'-'+ now.getTime() +".txt", (err) => {
+    fs.writeFile("backups/canvas - "+ now.getDate()+'-'+ now.getMonth()+'-'+ now.getFullYear()+'-'+ now.getTime() +".txt", JSON.stringify(canvas), (err) => {
         if (err) throw err;
         console.log('Backup made');
     });
@@ -50,16 +52,38 @@ if (fs.existsSync('canvas.txt')) {
 app.use(express.static("public"))
 
 io.on("connection", socket => {
+    messages[socket.id] = new Date();
+    pixels[socket.id] = new Date();
     socket.emit("canvas", canvas)
     
     socket.on("color", data => {
-        if(data.row <= CANVAS_ROWS && data.row > 0 && data.col <= CANVAS_COLS && data.col > 0){
-            canvas[data.row - 1][data.col - 1] = data.color
-            io.emit("canvas", canvas)
-            saveCanvas()
+        var now = new Date();
+        var seconds = (now.getTime() - pixels[socket.id].getTime()) / 1000;
+        if(seconds > 0.1 && data != "") {
+            if(data.row <= CANVAS_ROWS && data.row > 0 && data.col <= CANVAS_COLS && data.col > 0){
+                canvas[data.row - 1][data.col - 1] = data.color
+                io.emit("canvas", canvas)
+                pixels[socket.id] = new Date();
+            }
+        } else {
+            return
+        }
+    })
+    
+    socket.on("message", data => {
+        var now = new Date();
+        var seconds = (now.getTime() - messages[socket.id].getTime()) / 1000;
+        if(seconds > 1 && data != "") {
+            var message = data.message.replace(/(<([^>]+)>)/ig,"");
+            io.emit("newMessage", message)
+            messages[socket.id] = new Date();
+        } else {
+            return
         }
     })
 })
 
 setInterval(saveBackup, 1000 * 60 * 10);
+setInterval(saveCanvas, 1000 * 60 * 1);
+
 server.listen(3000)
